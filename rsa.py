a93c0beb9509cc5a2b15e840e1e9e36e8bc6f410
notmilhouse <gabrielwcs@hotmail.com>
2020-07-09 07:25:08 -0300

    Made advances on the modularization of the project. Integer encryption should now work just fine

--- rsa.py
+++ rsa.py
@@ -1,4 +1,15 @@
+"""
+TODO Divide in separate files accordingly to function (constant generators, encryption utilities, interactive version)
+TODO File handling and support
+TODO String encryption
+
+"""
+
+
 import math
+import os
+
+keys = {}
 
 def gen_N(p , q):
     return p * q
@@ -33,33 +44,87 @@ def gen_d(phi_N, e):
         d += 1
     return d-1
 
-def encrypt_message(m, e, N):
-    return pow(m, e) % N
+def gen_keys(p, q):
 
-def decrypt_message(c, d, N):
-    return pow(c, d) % N
-
-def main():
-    
-    #leitura dos primos
-    p = int(input("Insira o valor de p: "))
-
-    q = int(input("Insira o valor de q: "))
-
-    #definicao das constantes
     N = gen_N(p,q)
     phi_N = gen_phi_N(p,q)
     e = gen_e(phi_N, N)
     d = gen_d(phi_N, e)
 
-    print("N={}\nPhi(N)={}\ne={}\nd={}".format(N,phi_N,e,d))
+    keys['public'] = (e, N) 
+    keys['private'] = (d, N)
 
-    m = int(input("Insira a mensagem para encriptar: "))
-    c = pow(m,e) % N
+def encrypt_message(m, public_key):
+    if(not public_key):
+        return 0
+    return pow(m, public_key[0]) % public_key[1]
 
-    print("A mensagem encriptada eh: {}".format(c))
+def decrypt_message(c, private_key):
+    if(not private_key):
+        return 0
+    return pow(c, private_key[0]) % private_key[1]
 
-    print("Decriptando... {}".format(decrypt_message(c,d,N)))
+
+
+def main():
+    
+    while(True):
+        print("-- Choose one of the following --")
+        option = int(input("[1] - Setup RSA variables\n[2] - Encrypt a message\n[3] - Decrypt a message\n[0] - Exit"))
+
+        if(option == 1):
+            os.system("clear")
+            
+            print("-- Setup RSA Variables -- ")
+            
+            p = int(input("Insert the value for the first prime p: "))
+            q = int(input("Insert the value for the second prime q: "))
+        
+            gen_keys(p, q)
+
+        elif(option == 2):
+            os.system("clear")
+            
+            print("-- Message encryption -- ")
+
+            m = int(input("Insert a message to encrypt: "))
+            
+            if(keys):
+
+                c = encrypt_message(m, keys["public"])
+
+            else:
+
+                e = int(input("Insert the encryption key 'e': "))
+                N = int(input("Insert the public key 'N': "))
+
+                c = encrypt_message(m, (e, N))
+
+            print("The encrypted message is: {}".format(c))
+
+        elif(option == 3):
+
+            c = int(input("Insert a message do decrypt: "))
+
+            if(keys):
+                
+                m = decrypt_message(c, keys["private"])
+                
+            else:
+
+                d = int(input("Insert the decryption key 'd': "))
+                N = int(input("Insert the public key 'N': "))
+
+                m = decrypt_message(c, (d, N))
+
+            print("Decrypting to... {}".format(m))
+            
+        elif(option == 0):
+            break
+        else:
+            print("Invalid Option!")
+            os.system("clear")
+            continue
 
 
 if __name__ == "__main__":
